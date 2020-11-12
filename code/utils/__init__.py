209dd122b6cf68f964a7ff8625a074ef98d88e17
Dmitrij Vinokour <dimfred.1337@web.de>
2020-11-12 20:52:16 +0100

    data alex

--- code/utils/__init__.py
+++ code/utils/__init__.py
@@ -2,14 +2,16 @@ import cv2 as cv
 
 WINDOW_NAME = "img"
 
+
 def show(img, width=600):
     cv.namedWindow(WINDOW_NAME)
     cv.moveWindow(WINDOW_NAME, 100, 100)
     img = resize(img, width=width)
     cv.imshow(WINDOW_NAME, img)
-    while not ord('q') == cv.waitKey(200):
+    while not ord("q") == cv.waitKey(200):
         pass
     cv.destroyAllWindows()
+
 
 def resize(img, width: int = None, height: int = None, inter=cv.INTER_AREA):
     h, w = img.shape[:2]
