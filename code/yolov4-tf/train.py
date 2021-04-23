735734cc1183e409b51eb096aea19d63501624df
Dmitrij Vinokour <dimfred.1337@web.de>
2021-04-23 09:52:41 +0200

    al

--- code/yolov4-tf/train.py
+++ code/yolov4-tf/train.py
@@ -157,8 +157,10 @@ if __name__ == "__main__":
         n_workers=config.yolo.n_workers,
     )
 
+    # v = config.test_out_dir
+    valid_dir = config.test_out_dir
     valid_dataset = yolo.load_tfdataset(
-        dataset_path=config.valid_out_dir / "labels.txt",
+        dataset_path=valid_dir / "labels.txt",
         dataset_type=config.yolo.weights_type,
         preload=config.yolo.preload_dataset,
         training=False,
