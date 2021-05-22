38a9cdc18984956fe2d231ae17d2fd685ea94886
Dmitrij Vinokour <dimfred.1337@web.de>
2021-05-22 17:19:47 +0200

    asdf

--- code/utils/test_augs.py
+++ code/utils/test_augs.py
@@ -6,14 +6,30 @@ import utils
 from config import config
 
 # fmt:off
+alpha = 0.4
+lightness = 0.8
 sharpen = A.Compose([
-    A.Sharpen(always_apply=True)
+    A.Sharpen(
+        alpha=(alpha, alpha),
+        lightness=(lightness, lightness),
+        always_apply=True
+    )
+])
+clahe = A.Compose([
+    A.CLAHE(
+        always_apply=True
+    )
 ])
 # fmt:on
 
 bg_paths = utils.list_imgs(config.backgrounds_dir)
 for bg_path in bg_paths:
     img = cv.imread(str(bg_path))
-    sharpened = sharpen(image=img)["image"]
+    new_bg_path = bg_path.parent / f"{bg_path.stem}_clahe{bg_path.suffix}"
+    # sharpened = sharpen(image=img)["image"]
+    print(bg_path)
+    clahed = clahe(image=img)["image"]
 
-    utils.show(img, sharpened, superpix)
+    print(new_bg_path)
+    utils.show(img, clahed)
+    cv.imwrite(str(new_bg_path), clahed)
